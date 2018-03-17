b4d254e3132f435854f0a27fee186f6b5b63a7c0
Roman Levinzon <levinzon.roman@gmail.com>
2018-03-17 16:17:04 +0100

    detail menu

--- app/src/main/java/cz/levinzonr/stackquestions/screens/questiondetail/QuestionDetailFragment.kt
+++ app/src/main/java/cz/levinzonr/stackquestions/screens/questiondetail/QuestionDetailFragment.kt
@@ -2,15 +2,15 @@ package cz.levinzonr.stackquestions.screens.questiondetail
 
 
 import android.app.Application
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.text.Html
 import android.text.Spanned
 import android.util.Xml
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.webkit.MimeTypeMap
 import android.widget.Toast
 import com.squareup.picasso.Picasso
@@ -44,6 +44,7 @@ class QuestionDetailFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater!!.inflate(R.layout.fragment_question_detail, container, false)
     }
 
@@ -63,6 +64,35 @@ class QuestionDetailFragment : Fragment() {
         questiondetail_title.text = question.title
         question_body.text = question.body.fromHtml()
         Picasso.get().load(question.owner?.profileImage).into(author_image)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.detail_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val question : Question = arguments.getParcelable(BUNDLE_QUESTION)
+        return when (item?.itemId) {
+            R.id.action_share -> {
+                val shareIntent = Intent()
+                shareIntent.putExtra(Intent.EXTRA_TEXT, question.link)
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.type = "text/plain"
+                startActivity(Intent.createChooser(shareIntent, context.getString(R.string.share_msg)))
+                true
+            }
+           R.id.action_browser -> {
+               val viewIntent = Intent()
+               viewIntent.action = Intent.ACTION_VIEW
+               viewIntent.data = Uri.parse(question.link)
+               startActivity(viewIntent)
+               true
+           }
+           else -> {
+               throw IllegalArgumentException("Not supposed to happen")
+           }
+        }
     }
 
 }// Required empty public constructor
