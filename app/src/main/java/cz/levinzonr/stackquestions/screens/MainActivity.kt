0be7a852c90b2c06689348b869478240ec02fadc
Roman Levinzon <levinzon.roman@gmail.com>
2018-03-18 20:13:21 +0100

    unused menu removed

--- app/src/main/java/cz/levinzonr/stackquestions/screens/MainActivity.kt
+++ app/src/main/java/cz/levinzonr/stackquestions/screens/MainActivity.kt
@@ -37,22 +37,6 @@ class MainActivity : AppCompatActivity(), QuestionListFragment.OnListFragmentInt
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
         outState?.putBoolean(SAVED_IS_EMPTY, isEmptyView)
